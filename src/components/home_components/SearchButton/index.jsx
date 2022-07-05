82db685eb6e40d67518022b1e9a9c3926c7ff5bc
pedrooli98 <pedroblizz@outlook.com>
2022-07-05 17:09:35 -0300

    chore: estruturado os componentes da Home

---
+++ src/components/home_components/SearchButton/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Component = () => {
+  return (
+    <></>
+  )
+}
+
+export default Component
---
+++ src/components/home_components/SearchInput/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledComponent = styled.div`
+
+`
+
+export default StyledComponent;
