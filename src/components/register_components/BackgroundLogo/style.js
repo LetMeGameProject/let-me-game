c4fc7136a845c1af58e613b3de5a951706577171
therobite <antonielcamposm@gmail.com>
2022-07-12 21:26:26 -0300

    Fix: Ajustado responsividade do form login/register

--- src/components/register_components/BackgroundLogo/style.js
+++ src/components/register_components/BackgroundLogo/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const DivImages = styled.div`
   height: 100vh;
   width: 100vw;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url(${(prop) => prop.background});
   background-position: center;
   background-size: 100% auto;
