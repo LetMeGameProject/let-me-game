d2264049238688fb9a32c29d4c38a11dd389f7be
therobite <antonielcamposm@gmail.com>
2022-07-13 17:59:20 -0300

    Fix: ajustado proteção de rotas

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,9 @@
-import { useContext } from "react";
-import { Redirect } from "react-router-dom";
 import { BackgroundBody } from "../../components/login_components/BackgroundLogo";
 import { DivLogo } from "../../components/login_components/DivLogo";
 import { DivForm } from "../../components/login_components/FormLogin";
-import { LoggedUserContext } from "../../context/LoggedUser";
 
 export const Login = () => {
-  const { loggedUser } = useContext(LoggedUserContext);
-  return loggedUser ? (
-    <Redirect to="/home" />
-  ) : (
+  return (
     <BackgroundBody>
       <DivLogo />
       <DivForm />
